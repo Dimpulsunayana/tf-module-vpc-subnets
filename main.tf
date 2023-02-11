d9928ccbad87c0e616672b5c1ef4013a8c3dd73f
Raghu K <raghuk.vit@gmail.com>
2023-02-11 07:03:43 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -17,3 +17,9 @@ resource "aws_subnet" "main" {
   )
 }
 
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = "vpc-075435ce9af088bfa"
+  vpc_id        = aws_vpc.main.id
+}
+
