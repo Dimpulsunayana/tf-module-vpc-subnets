fcd5ee8c4d06e2c75d1595a1537514a8aea6d008
Raghu K <raghuk.vit@gmail.com>
2023-02-16 07:15:13 +0530

    Update tf-module-vpc/

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,3 @@ output "vpc_peering_connection_id" {
 output "public_subnet_ids" {
   value = module.public_subnets
 }
-
-output "one_subnet_id" {
-  value = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
-  //value = module.public_subnets["public"]["subnet_ids"][0]
-}
